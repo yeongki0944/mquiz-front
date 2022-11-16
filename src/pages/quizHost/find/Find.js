296805028617dbf0cc13b93c94b1c12a0d12b031
edcs960 <edcs960@naver.com>
2022-11-16 16:00:51 +0900

    [QHostFind] feat: add 퀴즈 조회 페이지 추가

--- src/pages/quizHost/find/Find.js
+++ src/pages/quizHost/find/Find.js
@@ -1,10 +1,92 @@
 import * as React from 'react';
+import Grid from "@mui/material/Grid";
+import {Button, FormControl, InputLabel, MenuItem, Select, TextField} from "@mui/material";
+import Box from "@mui/material/Box";
+import SearchIcon from '@mui/icons-material/Search';
+import {useState, useEffect} from "react";
 
 export default function Find(props) {
-
     return (
         <>
-            quizHostMain
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Nav/>
+                </Grid>
+                <Grid item xs={12} container spacing={2}>
+                    <Grid item xs={1}></Grid>
+                    <Grid item xs={10} container spacing={2}>
+                        <Grid item xs={12}>
+                            <QuizSearchBox />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <QuizSearchResultBox/>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={1}></Grid>
+                </Grid>
+                <Grid item xs={12}>
+                    footer
+                </Grid>
+            </Grid>
         </>
     );
+
+    function Nav() {
+        return (
+            <div style={{height:80}}><h1>네비게이션</h1></div>
+        );
+    }
+
+    function QuizSearchBox() {
+        // 나중에 리덕스로!
+        const [searchType, setSearchType] = useState("A");
+        const [searchText, setSearchText] = useState('');
+
+        return (
+            <Box sx={{textAlign:"center", marginTop:5}}>
+                <FormControl sx={{width:150, marginRight:1}}>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={searchType}
+                        onChange={(event) => {
+                            setSearchType(event.target.value);
+                        }}
+                    >
+                        <MenuItem value={"A"}>전체</MenuItem>
+                        <MenuItem value={"T"}>제목</MenuItem>
+                        <MenuItem value={"TG"}>태그</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField
+                    id="outlined-search"
+                    type="search"
+                    sx={{width:700, marginRight:1}}
+                    value={searchText}
+                    onChange={
+                    (event) => {
+                            setSearchText(event.target.value);
+                        }
+                    }
+                />
+                <Button
+                    variant="contained"
+                    sx={{height:55.9}}
+                    onClick={
+                        () => {console.log(searchText);}
+                    }
+                >
+                    <SearchIcon/>
+                </Button>
+            </Box>
+        );
+    }
+
+    function QuizSearchResultBox() {
+        return (
+            <Box sx={{textAlign:"center", marginTop:5}}>
+
+            </Box>
+        );
+    }
 }
