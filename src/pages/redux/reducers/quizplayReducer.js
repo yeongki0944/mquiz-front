966a72275a208993a3a0c93b2f93febcf886d999
junseak1105 <57184613+junseak1105@users.noreply.github.com>
2022-11-29 16:32:45 +0900

    [QPlay] feat: add Client의 OX퀴즈 버튼 기초 제작

--- src/pages/redux/reducers/quizplayReducer.js
+++ src/pages/redux/reducers/quizplayReducer.js
@@ -1,19 +1,51 @@
 import {createAction, handleActions} from "redux-actions";
 
+
+const SET_DATA = "SET_DATA";
+const SET_CONTENT = "SET_CONTENT";
 const SET_CURRENT_SHOW_PLAY = "SET_CURRENT_SHOW_PLAY";
 
+export const setData = createAction(SET_DATA);
+export const setContent = createAction(SET_CONTENT);
 export const R_setCurrentShow_play = createAction(SET_CURRENT_SHOW_PLAY);
+
+// 사용법
+// setData({key:'~', value:'~'})
+// setContent({key:'~', value:'~'})
 
 const initialState = {
     quizPlay:{
-        userInfo:{},
-        currentShow: 1,
-        answer:'',
-        result:{},
+        command: '',
+        sender: '',
+        quizNum : 1,
+        content : {
+            answer: '',
+            solved_time: 0,
+        }
     }
 }
 
 export const quizPlayReducer = handleActions({
+    //신버전
+    [SET_DATA]: (state, action) => {
+        return {
+            quizPlay: {
+                ...state.quizPlay,
+                [action.payload.key]: action.payload.value
+            }
+        }
+    },
+    [SET_CONTENT]: (state, action) => {
+        return {
+            quizPlay: {
+                ...state.quizPlay,
+                content: {
+                    ...state.quizPlay.content,
+                    [action.payload.key]: action.payload.value
+                }
+            }
+        }
+    },
     [SET_CURRENT_SHOW_PLAY]: (state, action) => {
         console.log(action.payload);
         return {
