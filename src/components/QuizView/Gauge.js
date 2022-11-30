dc7660f3e0793f0714774134b49bcc000944b84c
junseak1105 <57184613+junseak1105@users.noreply.github.com>
2022-11-30 12:43:40 +0900

    [Play] feat: fix Gauge 출력 범위 오류 수정

--- src/components/QuizView/Gauge.js
+++ src/components/QuizView/Gauge.js
@@ -13,7 +13,6 @@ const useStyles = makeStyles((theme) => ({
     },
     content: {
         width: "100%",
-        height: "100%",
         padding: theme.spacing(2),
         textAlign: "center",
         // verticalAlign: "middle",
