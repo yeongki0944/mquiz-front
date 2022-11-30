0250854d3c90317ed1280366e013d9cce328970c
junseak1105 <57184613+junseak1105@users.noreply.github.com>
2022-11-30 14:14:51 +0900

    [Play] feat: fix merge 오류 수정

--- src/components/QuizView/Gauge.js
+++ src/components/QuizView/Gauge.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
     },
     content: {
         width: "100%",
-        height: "100%",
+        // height: "100%",
         padding: theme.spacing(2),
         textAlign: "center",
         // verticalAlign: "middle",
