311f11e1ae2d69478637dcab1ca54ea524a6f628
김재훈 <57184613+junseak1105@users.noreply.github.com>
2022-12-13 23:59:46 +0900

    [Refactor] feat: update NavBar css 수정

--- src/components/quizHost/NavBar.js
+++ src/components/quizHost/NavBar.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import {BottomNavigation, BottomNavigationAction} from "@mui/material";
 import styled from "styled-components";
 import {useHistory} from "react-router-dom";
-import {Item_c} from "../../LayOuts/LayOuts";
+import {Content, Item, Item_c, Text} from "../../LayOuts/LayOuts";
 
 const Styled_NavBar = styled(BottomNavigation)`
     width: 100%;
@@ -19,19 +19,27 @@ const Styled_NavBar = styled(BottomNavigation)`
 export const NavBar = () => {
     const history = useHistory();
     return (
-            <Styled_NavBar showLabels>
-                <BottomNavigationAction label="Main" onClick={() => {
-                    history.push("/")
-                }}/>
-                <BottomNavigationAction label="DashBoard" onClick={() => {
-                    history.push("/QHost")
-                }}/>
-                <BottomNavigationAction label="Find" onClick={() => {
-                    history.push("/QHost/find")
-                }}/>
-                <BottomNavigationAction label="Report" onClick={() => {
-                    history.push("/QHost/report")
-                }}/>
-            </Styled_NavBar>
+        <Item sx={{place: 'center', width: "100%", height: '5vh',backgroundColor:'rgba(0,0,0,0.5)'}} sm={{position:'absolute',bottom:0}}>
+            <Item sx={{place: 'center'}} onClick={() => {
+                history.push("/")
+            }}>
+                <Text sx={{color:'#fff',cursor:'pointer'}}>Home</Text>
+            </Item>
+            <Item sx={{place: 'center'}} onClick={() => {
+                history.push("/QHost")
+            }}>
+                <Text sx={{color:'#fff',cursor:'pointer'}}>DashBoard</Text>
+            </Item>
+            <Item sx={{place: 'center',cursor:'pointer'}} onClick={() => {
+                history.push("/QHost/find")
+            }}>
+                <Text sx={{color:'#fff'}}>Find</Text>
+            </Item>
+            <Item sx={{place: 'center',cursor:'pointer'}} onClick={() => {
+                history.push("/QHost/report")
+            }}>
+                <Text sx={{color:'#fff'}}>Report</Text>
+            </Item>
+        </Item>
     )
 }
