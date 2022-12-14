a0055e8332750203cff46713c02a8d6112f5f0c8
edcs960 <edcs960@naver.com>
2022-12-14 15:09:40 +0900

    [Refactor] feat: update QR 코드 모달창 리펙토링

--- src/components/QR_Modal.js
+++ src/components/QR_Modal.js
@@ -1,22 +1,9 @@
 import Modal from "@mui/material/Modal";
-import {Item_c} from "../LayOuts/LayOuts";
+import {Item} from "../LayOuts/LayOuts";
 import QRCode from "react-qr-code";
 import * as React from "react";
-import Button from "@mui/material/Button";
-import CardMedia from "@mui/material/CardMedia";
-import styled from "styled-components";
 
-const Item_c_paper = styled(Item_c)`
-    background: #fff;
-    border-radius: 10px;
-    border: 1px solid #ccc;
-    padding: 20px;
-    box-shadow: 0 0 10px rgba(0,0,0,0.5);
-    margin: auto;
-    width: 400px;
-    height: 300px;
-    display: block;
-`
+
 export const QR_Modal = (props) => {
     return (
         <Modal
@@ -27,15 +14,26 @@ export const QR_Modal = (props) => {
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
-            <Item_c_paper>
-                <Item_c>
+            <Item sx={{
+                place:'center',
+                background: '#fff',
+                borderRadius: '10px',
+                border: '1px solid #ccc',
+                padding: '20px',
+                boxShadow: '0 0 10px rgba(0,0,0,0.5)',
+                margin: 'auto',
+                width: '400px',
+                height: '300px',
+                display: 'block'
+            }}>
+                <Item sx={{place:'center'}}>
                     <QRCode
                         size={256}
                         value={props.url}
                         viewBox={`0 0 256 256`}
                     />
-                </Item_c>
-            </Item_c_paper>
+                </Item>
+            </Item>
         </Modal>
     );
 }
